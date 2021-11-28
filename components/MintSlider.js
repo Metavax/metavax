0bd57196dba84d417cea8bee92b5e52b0d4ad72a
zayneanthonyrcd <zayne@rocketcitydigital.com>
2021-11-28 17:00:40 -0600

    Update

--- components/MintSlider.js
+++ components/MintSlider.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import Slider, { Handle } from "rc-slider";
 import "rc-slider/assets/index.css";
 import Image from "next/image";
@@ -7,7 +7,7 @@ import handle from "../public/imgs/virus-slider.png";
 
 export default function MintSlider() {
 	const [tokenCount, setTokenCount] = useState(1);
-	const mint = useCallback(() => {}, [tokenCount, setTokenCount]);
+	// const mint = useCallback(() => {}, [tokenCount, setTokenCount]);
 	return (
 		<>
 			<div className='max-w-xl mx-auto'>
@@ -18,9 +18,9 @@ export default function MintSlider() {
 					defaultValue={tokenCount}
 					handle={(handleProps) => (
 						<Handle {...handleProps} ariaValueTextFormatter={undefined}>
-							<div className='relative left-0 h-12 w-12'>
-								<div className='absolute bottom-12 flex justify-center items-center'>
-									<span className='absolute z-10 leading-none text-black text-xl mx-auto left-4 top-1'>
+							<div className='relative left-0 w-12 h-12'>
+								<div className='absolute flex items-center justify-center bottom-12'>
+									<span className='absolute z-10 mx-auto text-xl leading-none text-black left-4 top-1'>
 										{tokenCount}
 									</span>
 									<Image src={tooltip} alt='Slider Tooltip' />
