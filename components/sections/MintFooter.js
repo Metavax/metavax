02ba150c5fe45becd7d0f34f8ce8ddee1c71ec11
Zayne Anthony <zayneanthonyf@gmail.com>
2022-02-05 20:49:50 -0600

    Update

--- components/sections/MintFooter.js
+++ components/sections/MintFooter.js
@@ -45,7 +45,7 @@ export default function MintFooter() {
 
 							<div className='flex flex-row items-center justify-between w-full'>
 								<p className='mb-0'>Public Sale Price:</p>
-								<p>0.0069 ETH</p>
+								<p>0.069 ETH</p>
 							</div>
 						</Tile>
 						<Mint />
